3b2c07056aa0cd6280a2bda0b3505d003f1de0e8
RS782 <rodinms7@gmail.com>
2018-04-21 23:38:39 +0300

    admin dopolnenei 2

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,8 @@
 
 
 Route::group(['prefix'=>'admin','namespace'=>'Admin','middleware'=>['auth']], function (){
-Route::get('/','DashboardController@dashboard')->name('admin.index');
+    Route::get('/','DashboardController@dashboard')->name('admin.index');
+    Route::resource('/category', 'CategoryController', ['as'=>'admin']);
 });
 
 Route::get('/', function () {
